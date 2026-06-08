ccdb43c57c6cafbd459491ea023f7170037691d7
Vrushabh Shelke <vrushabhshelke@vionsys.com>
2026-06-08 12:59:50 +0530

    login screen changes

---
+++ app/login.tsx
@@ -0,0 +1,13 @@
+import Signin from "@/src/components/Signin";
+import React from "react";
+import { View } from "react-native";
+
+const login = () => {
+  return (
+    <View>
+      <Signin />
+    </View>
+  );
+};
+
+export default login;
